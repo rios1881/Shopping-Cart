537091a4839f53e34a1981e8a648eb1d412a2319
rios1881 <48953648+rios1881@users.noreply.github.com>
2019-05-02 09:51:14 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+var Names= [];
+var Prices= [];
+
+$(".Additem").click(function(){
+    //$("img").hide;
+     var price = $(".Prices").val();
+     var name = $(".Names").val(); 
+     Names.push(name);
+     Prices.push();
+     });
+
+ 
+$(".Checkout").click(function(){
+    //$("img").hide;
+ var price = $(".Prices").val();
+     var name = $(".Names").val(); 
+     Names.push(name);
+     Prices.push();
+     
+     var arraylength;
+     
+});
